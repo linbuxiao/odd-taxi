5aaaf2b3b7a140100ecf1fd9a807599f9d044956
biwenliang <linbuxiao@gmail.com>
2021-10-19 11:01:12 +0800

    deploy

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,11 @@
 import 'dotenv/config'
 import { Bot } from 'grammy'
 import { words } from './words.js'
+import { cache } from './cache.js'
 
 const bot = new Bot(process.env.BOT_TOKEN!)
 
-const stickers = await bot.api.getStickerSet('oddTaxii')
-
-const stickersId = stickers.stickers.map(item => item.file_id)
+const stickers = (await bot.api.getStickerSet('oddTaxii')).stickers.map(item => item.file_id)
 
 bot.command("start", (ctx) => ctx.reply("请问要去哪里呢"));
 
@@ -15,8 +14,17 @@ function rand (len: number) {
 }
 
 bot.on("message", async (ctx) => {
-  await bot.api.sendSticker(ctx.chat.id, stickersId[rand(stickersId.length)])
-  await bot.api.sendMessage(ctx.chat.id, words[rand(words.length)])
+  const { id } = ctx.chat
+  const pastChat = cache.read(id) 
+  let canPassWords = words
+  if(pastChat) {
+    canPassWords = words.filter(s => !pastChat.set.includes(s))
+  } 
+
+  const willSend = canPassWords[rand(canPassWords.length)]
+  cache.write(id, ctx.msg.date, willSend)
+  await bot.api.sendSticker(id, stickers[rand(stickers.length)])
+  await bot.api.sendMessage(id, willSend)
 });
 
 bot.start()
