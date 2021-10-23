733d8a12a7b63681163691e9e6768ceca050c985
linbuxiao <linbuxiao@gmail.com>
2021-10-23 23:14:33 +0800

    del: dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-import { Bot } from 'grammy';
-export declare const bot: Bot<import("grammy").Context, import("grammy").Api<import("grammy").RawApi>>;
-export declare const botApp: import("express-serve-static-core").Express;
--- dist/server.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { botApp } from './index.js';
-const app = express();
-app.use('/odd-taxi', botApp);
-app.listen(3010, () => {
-    console.log('Success!');
-});
