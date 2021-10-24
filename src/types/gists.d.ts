43354e0ed385e90b08c43003ad42ad9aa987d7bc
linbuxiao <linbuxiao@gmail.com>
2021-10-24 12:29:27 +0800

    fix: gists

--- src/types/gists.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'gists' {
-  type filesType = { files : { [key in string]: { content: string } }   }
-
-  export default class Gists {
-    constructor({token}: { token: string }) {}
-    async get(id: string) : Promise<{body: filesType}>
-    async edit(id: string, update: filesType): Promise<{body: { files: filesType }}>
-  }
-}
