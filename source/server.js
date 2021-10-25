0afb46d9900d6b3d996ad0c92e1f0ee2415c2017
linbuxiao <linbuxiao@gmail.com>
2021-10-25 21:59:05 +0800

    export default

--- source/server.js
+++ source/server.js
@@ -3,12 +3,10 @@ import http from 'http';
 import rawBody from 'raw-body';
 import { webhookCallback } from 'grammy';
 
-const server = http.createServer((async (req, res)=> {
+export default http.createServer((async (req, res)=> {
   rawBody(req, null, (err, body) => {
     req.body = JSON.parse(body)
     webhookCallback(bot, 'http')(req, res)
   })
-}))
-
-server.listen(3000)
+})).listen(3000)
 
