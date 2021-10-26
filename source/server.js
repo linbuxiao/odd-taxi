ec613c7aa170e91bdd56aa091880095ca39310fb
biwenliang <linbuxiao@gmail.com>
2021-10-26 09:57:51 +0800

    fix

--- source/server.js
+++ source/server.js
@@ -4,6 +4,11 @@ import rawBody from 'raw-body';
 import { webhookCallback } from 'grammy';
 
 http.createServer((async (req, res)=> {
+  if(req.method === 'GET') {
+    res.write('success~')
+    res.end()
+    return
+  }
   rawBody(req, null, (err, body) => {
     req.body = JSON.parse(body)
     webhookCallback(bot, 'http')(req, res)
