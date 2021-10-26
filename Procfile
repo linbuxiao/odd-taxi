c02b92c24c07f0c77713d035bf7b9c1c61054a25
biwenliang <linbuxiao@gmail.com>
2021-10-26 10:12:44 +0800

    fix

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: node source/server.js
--- source/bot.js
+++ source/bot.js
@@ -1,4 +1,4 @@
-// import 'dotenv/config'
+import 'dotenv/config'
 import { Bot } from 'grammy'
 import { db } from './words.js'
 import { cache } from './cache.js'
