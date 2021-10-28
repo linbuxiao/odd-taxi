5406475ae81977b236b04c4475a93ffccba83222
linbuxiao <linbuxiao@gmail.com>
2021-10-28 14:38:30 +0800

    upt: gist

--- gist.ts
+++ gist.ts
@@ -3,6 +3,6 @@ import Gist from 'https://raw.githubusercontent.com/linbuxiao/low_gist/main/mod.
 
 export async function fetchGist() {
   const gist = new Gist(Deno.env.get('GIST_TOKEN')!)
-  const gistResponse = await (await gist.get('1759dcfc56c42ed9cf214e46e4230de1')).json()
-  return gistResponse.files['data.json'].content
+  const gistResponse = await gist.get('1759dcfc56c42ed9cf214e46e4230de1')
+  return gistResponse.files!['data.json'].content
 }
