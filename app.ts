e9f2f5a3820f529d117e304bec51f50b20c1deb9
linbuxiao <linbuxiao@gmail.com>
2021-10-28 14:24:46 +0800

    deno

--- app.ts
+++ app.ts
@@ -1,19 +1,17 @@
 import { Cache } from 'https://raw.githubusercontent.com/linbuxiao/low-cache/main/mod.ts'
 import { Bot } from "https://deno.land/x/grammy/mod.ts"
 import { fetchGist } from './gist.ts'
-import { config as env } from "https://deno.land/x/dotenv/mod.ts";
 
-const ENV = env()
 
 const cache = new Cache({ttl: 300})
 
-const bot = new Bot(ENV['BOT_TOKEN']!)
+const bot = new Bot(Deno.env.get('BOT_TOKEN')!)
 
 const words = JSON.parse(await fetchGist())
 
 const stickersResponse = await bot.api.getStickerSet('oddTaxii')
 
-const stickers = stickersResponse.stickers.map(item => item.file_id)
+const stickers = stickersResponse.stickers.map((item) => item.file_id)
 
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)]
--- gist.ts
+++ gist.ts
@@ -1,10 +1,8 @@
 import Gist from 'https://raw.githubusercontent.com/linbuxiao/low_gist/main/mod.ts'
-import { config as env } from "https://deno.land/x/dotenv/mod.ts";
 
-const ENV = env()
 
 export async function fetchGist() {
-  const gist = new Gist(ENV['GIST_TOKEN'])
+  const gist = new Gist(Deno.env.get('GIST_TOKEN')!)
   const gistResponse = await (await gist.get('1759dcfc56c42ed9cf214e46e4230de1')).json()
   return gistResponse.files['data.json'].content
 }
